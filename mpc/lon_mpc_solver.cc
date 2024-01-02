c6b9d14060e5677f416b91cbac5596a3865e0519
LLLLLyra <>
2024-01-02 22:50:50 +0800

    fix: triu P

--- mpc/lon_mpc_solver.cc
+++ mpc/lon_mpc_solver.cc
@@ -63,7 +63,7 @@ void LongitudinalMPCSolver::CalculateKernel(std::vector<OSQPFloat>* P_data,
                                             std::vector<OSQPInt>* P_indices,
                                             std::vector<OSQPInt>* P_indptr) {
   const size_t kNumParam = num_of_state_ * (horizon_ + 1) +
-                           num_of_state_ * horizon_ +
+                           num_of_control_ * horizon_ +
                            num_of_slack_var_ * (horizon_ + 1);
 
   CompensateSlackWeights(&diag_matrix_w_s_slack_l_);
@@ -85,18 +85,21 @@ void LongitudinalMPCSolver::CalculateKernel(std::vector<OSQPFloat>* P_data,
 
   // Q_n
   columns[index].emplace_back(index, matrix_q_n_(0, 0));
-  columns[index].emplace_back(index + 1, matrix_q_n_(1, 0));
+  // columns[index].emplace_back(index + 1, matrix_q_n_(1, 0));
   index++;
   columns[index].emplace_back(index, matrix_q_n_(1, 1));
   columns[index].emplace_back(index - 1, matrix_q_n_(0, 1));
+
+  CHECK_EQ(++index, num_of_state_ * (horizon_ + 1));
 
   // control
   const double dt_squared = delta_t_ * delta_t_;
   columns[num_of_state_ * (horizon_ + 1)].emplace_back(
       num_of_state_ * (horizon_ + 1),
       diag_matrix_r_[0] + 2.0 * diag_matrix_r_dot_[0] / dt_squared);
-  columns[num_of_state_ * (horizon_ + 1)].emplace_back(
-      num_of_state_ * (horizon_ + 1) + 1, -diag_matrix_r_dot_[0] / dt_squared);
+  // columns[num_of_state_ * (horizon_ + 1)].emplace_back(
+  //     num_of_state_ * (horizon_ + 1) + 1, -diag_matrix_r_dot_[0] /
+  //     dt_squared);
 
   for (size_t i = 1; i + 1 < horizon_; ++i) {
     columns[i + num_of_state_ * (horizon_ + 1)].emplace_back(
@@ -105,9 +108,9 @@ void LongitudinalMPCSolver::CalculateKernel(std::vector<OSQPFloat>* P_data,
     columns[i + num_of_state_ * (horizon_ + 1)].emplace_back(
         i + num_of_state_ * (horizon_ + 1) - 1,
         -diag_matrix_r_dot_[0] / dt_squared);
-    columns[i + num_of_state_ * (horizon_ + 1)].emplace_back(
-        i + num_of_state_ * (horizon_ + 1) + 1,
-        -diag_matrix_r_dot_[0] / dt_squared);
+    // columns[i + num_of_state_ * (horizon_ + 1)].emplace_back(
+    //     i + num_of_state_ * (horizon_ + 1) + 1,
+    //     -diag_matrix_r_dot_[0] / dt_squared);
   }
 
   columns[num_of_state_ * (horizon_ + 1) + num_of_control_ * horizon_ - 1]
@@ -120,24 +123,23 @@ void LongitudinalMPCSolver::CalculateKernel(std::vector<OSQPFloat>* P_data,
           -diag_matrix_r_dot_[0] / dt_squared);
 
   // slack var
-  index = num_of_state_ * (horizon_ + 1) + num_of_state_ * horizon_;
+  index = num_of_state_ * (horizon_ + 1) + num_of_control_ * horizon_;
   for (size_t i = 0; i < horizon_ + 1; ++i) {
     if (diag_matrix_w_s_slack_u_[i] != 0.0) {
       columns[index + i].emplace_back(index + i, diag_matrix_w_s_slack_u_[i]);
     }
     if (diag_matrix_w_ds_slack_u_[i] != 0.0) {
-      columns[index + i + (horizon_ + 1) * 2].emplace_back(
-          index + i + (horizon_ + 1) * 2, diag_matrix_w_ds_slack_u_[i]);
+      columns[index + i + (horizon_ + 1) * 1].emplace_back(
+          index + i + (horizon_ + 1) * 1, diag_matrix_w_ds_slack_u_[i]);
     }
     if (diag_matrix_w_s_slack_l_[i] != 0.0) {
-      columns[index + i + horizon_ + 1].emplace_back(
-          index + i + horizon_ + 1, diag_matrix_w_s_slack_l_[i]);
+      columns[index + i + (horizon_ + 1) * 2].emplace_back(
+          index + i + (horizon_ + 1) * 2, diag_matrix_w_s_slack_l_[i]);
     }
     if (diag_matrix_w_ds_slack_l_[i] != 0.0) {
       columns[index + i + (horizon_ + 1) * 3].emplace_back(
           index + i + (horizon_ + 1) * 3, diag_matrix_w_ds_slack_l_[i]);
     }
-    ++index;
   }
 
   CSCInitHelper<double>(columns, P_data, P_indices, P_indptr);
@@ -145,7 +147,7 @@ void LongitudinalMPCSolver::CalculateKernel(std::vector<OSQPFloat>* P_data,
 
 void LongitudinalMPCSolver::CalculateOffset(std::vector<OSQPFloat>* q) {
   const size_t kNumParam = num_of_state_ * (horizon_ + 1) +
-                           num_of_state_ * horizon_ +
+                           num_of_control_ * horizon_ +
                            num_of_slack_var_ * (horizon_ + 1);
   q->resize(kNumParam);
   int index = 0;
